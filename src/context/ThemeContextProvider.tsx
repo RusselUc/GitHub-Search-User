e14c5365d308e909913fc9d2405f15c6186d6ef1
RusselUc <6584@itescam.edu.mx>
2023-02-20 17:10:31 -0600

    my first commit

---
+++ src/context/ThemeContextProvider.tsx
@@ -0,0 +1,56 @@
+import React, { createContext, useEffect, useState } from 'react'
+import { ThemeProvider } from 'styled-components';
+
+interface ThemeContextProps {
+    changeTheme: () => void;
+    lightMode: boolean;
+}
+
+export const ThemeContext = createContext({} as ThemeContextProps);
+
+const ThemeContextProvider = (props:any) => {
+
+    const [lightMode, setLightMode] = useState<boolean>(false);
+
+    const darkTheme: object = {
+        colors: {
+          background: "#141D2F",
+          themeBtn: "#fff",
+          card: "#1E2A47",
+          textNorm: "#fff",
+          textBolded: "#FFF",
+        },
+      };
+
+      const lightTheme: object = {
+        colors: {
+          background: "#F6F8FF",
+          themeBtn: "#4B6A9B",
+          card: "#FEFEFE",
+          textNorm: "#697C9A",
+          textBolded: "#2B3442",
+        },
+      };
+
+      const changeTheme = () => {
+        setLightMode(prev => !prev);
+      }
+
+      useEffect(() => {
+        localStorage.getItem("theme") === "ligth" && setLightMode(true)
+      },[])
+
+      useEffect(() => {
+        const mode = lightMode ? "light" : "dark"
+      },[])
+
+  return (
+    <ThemeContext.Provider value={{changeTheme, lightMode}}>
+      <ThemeProvider theme={lightMode ? lightTheme : darkTheme}>
+        {props.children}
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  )
+}
+
+export default ThemeContextProvider
